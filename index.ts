5e65606888474f971695999e974062799e408294
Lucas Boinet <boinet1906@gmail.com>
2024-04-19 22:44:56 +0200

    feat: add prometheus metrics client

--- index.ts
+++ index.ts
@@ -10,6 +10,21 @@ import config from './config';
 
 dotenv.config();
 
+const register = new promClient.Registry();
+register.setDefaultLabels({
+  app: 'gamerhub_api'
+});
+promClient.collectDefaultMetrics({ register });
+
+const httpRequestDurationMicroseconds = new promClient.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in microseconds',
+  labelNames: ['method', 'route', 'code'],
+  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
+});
+
+register.registerMetric(httpRequestDurationMicroseconds);
+
 const app: Application = express();
 
 app.use(express.json());
@@ -26,22 +41,6 @@ app.get('/', (req, res) => {
 app.use('/api', router);
 
 const server = http.createServer(app);
-
-const register = new promClient.Registry();
-promClient.collectDefaultMetrics({ 
-  register, 
-  labels: { app: 'gamerhub_api' },
-  prefix: 'gamerhub_api_' 
-});
-
-const httpRequestDurationMicroseconds = new promClient.Histogram({
-  name: 'http_request_duration_seconds',
-  help: 'Duration of HTTP requests in microseconds',
-  labelNames: ['method', 'route', 'code'],
-  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
-});
-
-register.registerMetric(httpRequestDurationMicroseconds);
 
 database.connect();
 
