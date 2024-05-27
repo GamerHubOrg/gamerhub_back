660c0355c7fefce25253cfc0977c2afa4d4f2e73
Lucas B <lucas@skyloud.fr>
2024-05-27 12:29:39 +0200

    feat: fix config origin

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  origin: 'http://localhost:5174',
+  origin: process.env.ORIGIN || 'http://localhost:5174',
   database: {
     url: process.env.MONGO_URI || 'mon  godb://admin:admin@mongo:27017/database?authSource=admin',
   },
