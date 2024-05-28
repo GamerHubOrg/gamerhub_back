da4e5bc08216f80edd63a555255ae22504449bb8
Lucas B <lucas@skyloud.fr>
2024-05-28 09:26:13 +0200

    chore: add vote action for undercover

--- socket/games/undercover/handler.ts
+++ socket/games/undercover/handler.ts
@@ -1,6 +1,6 @@
 import { roomsDataMap } from "../../room-handler";
 import { IoType, SocketType, SocketUser } from "../../types";
-import { IUndercoverGameData, IUndercoverSendWord } from "./types";
+import { IUndercoverConfig, IUndercoverGameData, IUndercoverSendVote, IUndercoverSendWord, defaultUndercoverGameData } from "./types";
 
 // Socket handlers
 const UndercoverHandler = (io: IoType, socket: SocketType) => {
@@ -8,7 +8,7 @@ const UndercoverHandler = (io: IoType, socket: SocketType) => {
     const roomData = roomsDataMap.get(roomId);
     if (!roomData) return socket.emit("room:not-found", roomId);
 
-    const gameData: IUndercoverGameData = roomData.gameData || {};
+    const gameData = (roomData.gameData as IUndercoverGameData) || defaultUndercoverGameData;
     const words = (gameData as IUndercoverGameData).words || [];
     const randomPlayer = roomData.users[0].id;
 
@@ -21,10 +21,11 @@ const UndercoverHandler = (io: IoType, socket: SocketType) => {
     const roomData = roomsDataMap.get(roomId);
     if (!roomData) return socket.emit("room:not-found", roomId);
 
-    const gameData: IUndercoverGameData = roomData.gameData || {};
-    const words = (gameData as IUndercoverGameData).words || [];
+    const gameData = (roomData.gameData as IUndercoverGameData) || defaultUndercoverGameData;
+    const gameConfig = (roomData.config as IUndercoverConfig) || { wordsPerTurn: 1 };
+    const words = gameData.words || [];
     const currentPlayerIndex = roomData.users.findIndex((u: SocketUser) => u.id === userId);
-    const randomPlayer = roomData.users[currentPlayerIndex] ? roomData.users[currentPlayerIndex].id : roomData.users[0].id;
+    const randomPlayer = roomData.users[currentPlayerIndex + 1] ? roomData.users[currentPlayerIndex + 1].id : roomData.users[0].id;
 
     words.push({
         playerId: userId,
@@ -33,11 +34,35 @@ const UndercoverHandler = (io: IoType, socket: SocketType) => {
 
     gameData.playerTurn = randomPlayer;
     gameData.words = words;
+
+    if (words.length / roomData.users.length === gameConfig.wordsPerTurn) {
+      gameData.state = 'vote';
+    }
+
+    io.in(roomId).emit("game:undercover:data", { data: gameData });
+  };
+
+  const onVote = ({ roomId, userId, vote }: IUndercoverSendVote) => {
+    const roomData = roomsDataMap.get(roomId);
+    if (!roomData) return socket.emit("room:not-found", roomId);
+
+    const gameData = (roomData.gameData as IUndercoverGameData) || defaultUndercoverGameData;
+    const votes = gameData.votes || [];
+
+    votes.push({ playerId: userId, vote, turn: gameData.turn })
+
+    if ((votes.length / gameData.turn) === roomData.users.length) {
+      gameData.state = 'words';
+    }
+
+    gameData.votes = votes;
+
     io.in(roomId).emit("game:undercover:data", { data: gameData });
   };
 
   socket.on("game:undercover:initialize", onInitialize);
   socket.on("game:undercover:send-word", onSendWord);
+  socket.on("game:undercover:vote", onVote);
 };
 
 export default UndercoverHandler;
--- socket/games/undercover/types.ts
+++ socket/games/undercover/types.ts
@@ -1,8 +1,13 @@
-import { IGameData } from "../../types";
+import { IGameData, IRoomConfig } from "../../types";
+
+export type IUndercoverGameState = 'vote' | 'words';
 
 export interface IUndercoverGameData extends IGameData {
-    words?: IUndercoverWords[]; 
+    words?: IUndercoverWords[];
+    votes: IUndercoverVote[],
     playerTurn?: string;
+    state: IUndercoverGameState;
+    turn: number;
 }
 
 export interface IUndercoverSendWord {
@@ -16,6 +21,20 @@ export interface IUndercoverWords {
     word: string;
 }
 
-export interface IUndercoverConfig {
-    wordsPerTurn: number,
+export interface IUndercoverSendVote {
+    roomId: string,
+    vote: string;
+    userId: string,
 }
+
+export interface IUndercoverVote {
+    playerId: string;
+    vote: string;
+    turn: number;
+}
+
+export interface IUndercoverConfig extends IRoomConfig {
+    wordsPerTurn: number;
+}
+
+export const defaultUndercoverGameData: IUndercoverGameData = { state: 'words', turn: 1, votes: [] };
