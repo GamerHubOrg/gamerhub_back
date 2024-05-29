d1c9957fbde54eea98630babd08ec7cea490bdbe
Lucas B <lucas@skyloud.fr>
2024-05-29 14:46:17 +0200

    feat: add config for each games

--- utils/functions.ts
+++ utils/functions.ts
@@ -11,6 +11,10 @@ export const generateRandomString = (numberOfDigit = 6) : string => {
     return randomString;
 }
 
-export const getRandomIndex = (list: Array<any>): number => {
-  return Math.floor(Math.random() * list.length);
+export const getRandomElement = (list: Array<any>, count: number = 1): any => {
+  const shuffled = list.sort(() => 0.5 - Math.random());
+  if (count === list.length) return shuffled;
+  if (count > list.length) return shuffled.slice(0, 1)[0];
+  const selected = shuffled.slice(0, count);
+  return count === 1 ? selected[0] : selected;
 }
