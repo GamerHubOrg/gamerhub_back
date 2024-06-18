dde2f9e49407130c579cd28246d23ce83b06fe5a
Lucas B <lucas@skyloud.fr>
2024-06-18 14:52:56 +0200

    feat: add logs to undercover game

--- socket/logs-handler.ts
+++ socket/logs-handler.ts
@@ -1,6 +1,6 @@
 import { IRoomData, IRoomLog, SocketUser } from "./types";
 
-export default class RoomLogger {
+export class RoomLogger {
   public onRoomCreate(roomId: string, user: SocketUser): IRoomLog[] {
     return [
       { date: new Date(), message: `The room ${roomId} has been created.` },
@@ -58,6 +58,48 @@ export default class RoomLogger {
       {
         date: new Date(),
         message: `The game has changed to ${roomData.gameName}.`,
+      },
+    ];
+  }
+}
+
+export class UndercoverLogger {
+  public onSendWord(roomData: IRoomData, user: SocketUser, word: string) {
+    roomData.logs = [
+      ...roomData.logs,
+      {
+        date: new Date(),
+        message: `${user.username} gave the word : ${word}`,
+      },
+    ];
+  }
+
+  public onCanVote(roomData: IRoomData) {
+    roomData.logs = [
+      ...roomData.logs,
+      {
+        date: new Date(),
+        message: 'Players can now vote to find the undercover',
+      },
+    ];
+  }
+
+  public onVote(roomData: IRoomData, user: SocketUser, vote: SocketUser) {
+    roomData.logs = [
+      ...roomData.logs,
+      {
+        date: new Date(),
+        message: `${user.username} voted for ${vote.username}`,
+      },
+    ];
+  }
+
+  public onSideWin(roomData: IRoomData, side: string) {
+    roomData.logs = [
+      ...roomData.logs,
+      {
+        date: new Date(),
+        message: `Game ended, ${side} win`,
       },
     ];
   }
