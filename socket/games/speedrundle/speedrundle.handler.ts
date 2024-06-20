50542fb03675a307c0a98a55404de09ad36b357f
KenTay7 <johnkta7@gmail.com>
2024-06-20 16:44:54 +0200

    fix: Select resetting input on other player's guess

--- socket/games/speedrundle/speedrundle.handler.ts
+++ socket/games/speedrundle/speedrundle.handler.ts
@@ -32,8 +32,6 @@ const calculateScore = (time : number, nbTries : number) => {
   const ratio = (nbTries / time);
   if (ratio > 0.4) reduction += 20 / ratio;
 
-  console.log(nbTries, time, ratio);
-
   const reducedScore = baseScore - reduction;
   return Math.max(Math.round(reducedScore), 100);
 };
@@ -111,77 +109,8 @@ const SpeedrundleHandler = (io: IoType, socket: SocketType) => {
       }
     }
 
-    io.in(roomId).emit("game:speedrundle:data", { data: gameData });
+    socket.emit("game:speedrundle:data", { data: gameData });
   };
-
-  // const onVote = ({ roomId, userId, vote }: ISpeedrundleSendVote) => {
-  //   const roomData = (roomsDataMap.get(roomId) as ISpeedrundleRoomData);
-  //   if (!roomData) return socket.emit("room:not-found", roomId);
-
-  //   const gameData = roomData.gameData || defaultSpeedrundleGameData;
-  //   const votes = gameData.votes || [];
-  //   const users = roomData.users || [];
-  //   const gameTurn = gameData.turn || 1;
-  //   const usersThatCanPlay = roomData.users.filter((u) => !u.isEliminated);
-
-  //   votes.push({ playerId: userId, vote })
-  //   gameData.votes = votes;
-
-  //   if (votes.length !== usersThatCanPlay.length) {
-  //     io.in(roomId).emit("game:speedrundle:data", { data: gameData });
-  //     return;
-  //   }
-
-  //   const mostVotedPlayer = votes.reduce((acc: any, vote: ISpeedrundleVote) => {
-  //     const voteNumber = votes.filter((v) => v.vote === vote.vote).length;
-  //     if (!acc) return undefined
-  //     if (acc.count > 0 && acc.count === voteNumber && acc.vote !== vote.vote) return undefined;
-
-  //     return acc.count > voteNumber ? acc : { ...vote, count: voteNumber };
-  //   }, { count: 0 })
-
-  //   const isVoteTied = !mostVotedPlayer;
-  //   const isMostVotedSpeedrundle = !isVoteTied && gameData.undercoverPlayerIds?.includes(mostVotedPlayer.vote);
-
-  //   if (!isVoteTied && isMostVotedSpeedrundle) {
-  //     roomData.gameState = 'results';
-  //     gameData.campWin = 'civilian';
-  //     io.in(roomId).emit("game:speedrundle:data", { data: gameData });
-  //     io.in(roomId).emit("room:updated", roomData);
-  //     return;
-  //   }
-
-  //   if (!isVoteTied && !isMostVotedSpeedrundle) {
-  //     const votedPlayerIndex = users.findIndex((u) => u.id === mostVotedPlayer.vote);
-  //     if (votedPlayerIndex === -1) return;
-  //     const user = roomData.users[votedPlayerIndex];
-  //     roomData.users[votedPlayerIndex] = {
-  //       ...user,
-  //       isEliminated: true
-  //     }
-
-  //     const notEliminatedPlayers = users.filter((u) => !u.isEliminated).length;
-
-  //     if (notEliminatedPlayers < 3) {
-  //       roomData.gameState = 'results';
-  //       gameData.campWin = 'undercover';
-  //       io.in(roomId).emit("game:speedrundle:data", { data: gameData });
-  //       io.in(roomId).emit("room:updated", roomData);
-  //       return;
-  //     }
-  //   }
-
-  //   const usersNotEliminated = roomData.users.filter((u) => !u.isEliminated);
-  //   const currentPlayerIndex = usersNotEliminated.findIndex((u: SocketUser) => u.id === gameData.playerTurn);
-  //   const randomPlayer = usersNotEliminated[currentPlayerIndex + 1] ? usersNotEliminated[currentPlayerIndex + 1].id : usersNotEliminated[0].id;
-
-  //   gameData.playerTurn = randomPlayer;
-  //   gameData.state = 'words';
-  //   gameData.turn = gameTurn + 1;
-  //   gameData.votes = [];
-  //   io.in(roomId).emit("game:speedrundle:data", { data: gameData });
-  //   io.in(roomId).emit("room:updated", roomData);
-  // };
 
   const onGetData = (roomId: string) => {
     const roomData = roomsDataMap.get(roomId);
@@ -193,7 +122,6 @@ const SpeedrundleHandler = (io: IoType, socket: SocketType) => {
   socket.on("game:speedrundle:initialize", onInitialize);
   socket.on("game:speedrundle:guess", onGuess);
   socket.on("game:speedrundle:get-data", onGetData);
-  // socket.on("game:speedrundle:vote", onVote);
 };
 
 export default SpeedrundleHandler;
