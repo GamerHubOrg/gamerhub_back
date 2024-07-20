66d5e82453f5d89d942999c0b7f1263f137842bc
Lucas Boinet <boinet1906@gmail.com>
2024-07-20 10:56:12 +0200

    refacto: werewolves handler organization

--- socket/games/werewolves/werewolves.handler.ts
+++ socket/games/werewolves/werewolves.handler.ts
@@ -22,10 +22,10 @@ const WerewolvesHandler = (io: IoType, socket: SocketType) => {
   socket.on("game:werewolves:vote:tmp", ({ roomId, userId, target }: IWerewolvesSendTarget) => onWolfVoteSelectPlayer(io, socket)({ roomId, userId, target }));
   socket.on("game:werewolves:village:vote", ({ roomId, userId, target }: IWerewolvesSendTarget) => onVillageVote(io, socket)({ roomId, userId, target }));
   socket.on("game:werewolves:wolf:vote", ({ roomId, userId, target }: IWerewolvesSendTarget) => onWolfVote(io, socket)({ roomId, userId, target }));
-  socket.on("game:werewolves:witch:save", ({ roomId, userId: playerId, target }: IWerewolvesSendTarget) => onWitchSavePlayer(io, socket)({ roomId, userId: playerId, target }));
-  socket.on("game:werewolves:witch:kill", ({ roomId, userId: playerId, target }: IWerewolvesSendTarget) => onWitchKillPlayer(io, socket)({ roomId, userId: playerId, target }));
+  socket.on("game:werewolves:witch:save", ({ roomId, userId, target }: IWerewolvesSendTarget) => onWitchSavePlayer(io, socket)({ roomId, userId, target }));
+  socket.on("game:werewolves:witch:kill", ({ roomId, userId, target }: IWerewolvesSendTarget) => onWitchKillPlayer(io, socket)({ roomId, userId, target }));
   socket.on("game:werewolves:witch:skip", ({ roomId, playerId }: { roomId: string, playerId: string }) => onWitchSkip(io, socket)({ roomId, playerId }));
-  socket.on("game:werewolves:hunter:kill", ({ roomId, userId: playerId, target } : IWerewolvesSendTarget) => onHunterKillPlayer(io, socket)({ roomId, userId: playerId, target }));
+  socket.on("game:werewolves:hunter:kill", ({ roomId, userId, target } : IWerewolvesSendTarget) => onHunterKillPlayer(io, socket)({ roomId, userId, target }));
   socket.on("game:werewolves:psychic:watch", ({ roomId, userId, target }: IWerewolvesSendTarget) => onPsychicWatchRole(io, socket)({ roomId, userId, target }));
   socket.on("game:werewolves:cupidon:couple", ({ roomId, playerId, couple }: IWerewolvesSendCouple) => onCupidonDefineCouple(io, socket)({ roomId, playerId, couple }));
   socket.on("game:werewolves:thief:choose", ({ roomId, userId, target }: IWerewolvesSendTarget) => onThiefChooseRole(io, socket)({ roomId, userId, target }));
