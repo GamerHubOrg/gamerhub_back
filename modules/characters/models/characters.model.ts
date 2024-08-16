fdd8e7d388a4bf0d7a2fa7930547df9ee98092f0
KenTay7 <johnkta7@gmail.com>
2024-08-16 17:53:27 +0200

    feat: Handle speedrundle characters language names

--- modules/characters/models/characters.model.ts
+++ modules/characters/models/characters.model.ts
@@ -2,16 +2,27 @@ import mongoose, { Schema } from "mongoose";
 import LolSchema from "./lol-characters.model";
 import PokemonSchema from "./pokemon-characters.model";
 
+const NameSchema = new mongoose.Schema(
+  {
+    fr: { type: String },
+    en: { type: String },
+  },
+  { _id: false }
+);
+
 const CharacterSchema = new Schema(
   {
     name: { type: String, required: true },
+    names: {
+      type: NameSchema,
+    },
     lang: {
       type: String,
       required: true,
     },
     apiId: {
       type: String,
-      required : true,
+      required: true,
       unique: true,
       immutable: true,
     },
@@ -24,7 +35,13 @@ const CharacterSchema = new Schema(
 
 const CharacterModel = mongoose.model("Character", CharacterSchema);
 
-export const LolCharacterModel = CharacterModel.discriminator("league_of_legends", LolSchema);
-export const PokemonCharacterModel = CharacterModel.discriminator("pokemon", PokemonSchema);
+export const LolCharacterModel = CharacterModel.discriminator(
+  "league_of_legends",
+  LolSchema
+);
+export const PokemonCharacterModel = CharacterModel.discriminator(
+  "pokemon",
+  PokemonSchema
+);
 
 export default CharacterModel;
